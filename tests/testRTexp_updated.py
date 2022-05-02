ba98277643df84853bfab9cd4f12fa2cf8e1dd78
Ricmatgal <riccardomgalli@gmail.com>
2022-05-02 15:59:34 +0200

    Added updated test script

---
+++ tests/testRTexp_updated.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+"""
+Real time export simulation
+
+__________________________________________________________________________
+Copyright (C) 2016-2021 OpenNFT.org
+
+Written by Artem Nikonorov, Yury Koush
+"""
+
+
+import os
+import shutil
+from time import sleep
+import glob
+
+delete_files = True
+
+#mask = "001_000005_00O"
+#fns = list(range(1,211))
+#fns = list(range(1,93))
+fns = None
+testCase = 'PSC'
+if testCase == 'PSC':
+    #srcpath = 'F:/MRI_DATA/20201119.LUCACHU_S20_SESS1.99622'
+    #dstpath = 'F:/rtTestFolder'
+    #srcpath = 'C:/Users/gallir/Documents/OPENNFT/ProjectCecilia/dummy_data/20201119.LUCACHU_S20_SESS1.99622'
+    #srcpath = 'C:/Users/gallir/Documents/OPENNFT/ProjectCecilia/dummy_data/testpath'
+    srcpath = 'C:/Users/gallir/Documents/OPENNFT/ProjectCecilia/dummy_data/20220430.MRI_DEV_Test_NFB.110734'
+    dstpath = 'C:/Users/gallir/Documents/OPENNFT/ProjectCecilia/dummy_data/rtTestFolder'
+   # srcpath = 'E:/rtQA_testing/data/rtDCM/NFB_r2'
+   # dstpath = 'E:/rtTestFolder'
+    #pause_in_sec = 0.8
+    pause_in_sec = 0.8
+elif testCase == 'SVM':
+    srcpath = 'C:/_RT/rtData/NF_SVM/NF_Run_1_src'
+    dstpath = 'C:/_RT/rtData/NF_SVM/NF_Run_1'
+    pause_in_sec = 1
+
+elif testCase == 'DCM':
+    srcpath = 'C:/_RT/rtData/NF_DCM/NF_Run_1_src'
+    dstpath = 'C:/_RT/rtData/NF_DCM/NF_Run_1'
+    pause_in_sec = 1
+
+if delete_files:
+    files = glob.glob(dstpath+'/*')
+    for f in files:
+        os.remove(f)
+
+if fns is None:
+    filelist = os.listdir(srcpath)
+    #print(filelist)
+else:
+    filelist = []
+    for fn in fns:
+        fname = "{0}{1:03d}.dcm".format(mask, fn)
+        filelist.append(fname)
+
+for filename in filelist:
+    src = os.path.join(srcpath, filename)
+    if os.path.isfile(src):
+        dst = os.path.join(dstpath, filename)
+        shutil.copy(src, dst)
+        print(filename)
+        sleep(pause_in_sec) # seconds
